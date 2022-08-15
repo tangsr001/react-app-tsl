e8f86a383e82f454d498c2dbd7eb55f148665840
tom <813540223@qq.com>
2022-08-15 22:25:48 +0800

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+// 所有接口方法的列表
+import Ajax from './ajax.js'
+
+export const reqmain = () => {
+    // 默认传了 GET
+    return Ajax('/tesla')
+}
+
+export const reqshop = () => {
+    return Ajax('/shop')
+}
+
+export const reqfind = (page) => {
+    return Ajax(`/find/${page}`)
+}
---
+++ src/pages/Find/index.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect, memo } from "react";
+import Scroll from '../../baseUI/scroll'
+import { connect } from 'react-redux'
+import { actionCreators } from './store'
+import { Main } from "./index.style";
+import Lazyload, { forceCheck } from 'react-lazyload'
+import loading from '../../assets/Images/1.gif'
+import loading2 from '../../assets/Images/loading.gif'
+import { useHistory } from "react-router-dom";
+import { renderRoutes } from 'react-router-config';
+
+
+const Find = (props) => {
+
+  const history = useHistory()
+
+  const { route } = props
+
+  let [page, setPage] = useState(1)
+
+  // state
+  const { finddata } = props
+
+  // action
+  const { getFindDataDispatch } = props
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { newsList = [] } = finddata
+
+  // 判断仓库是否为空，空则getFindDataDispatch()，page改变重新渲染页面
+  useEffect(() => {
+    if (!finddata.length) {
+      getFindDataDispatch(page)
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 500)
+    }
+  }, [page])
+
+  // 上拉加载更多
+  const handlePullUp = () => {
+    if (isLoading) return;
+    setPage(++page)
+    setIsLoading(true)
+  }
+
+  // 下拉刷新
+  const handlePullDown = () => {
+
+  }
+
+  const gotoNews = (item) => {
+    history.push({ pathname: `/find/${item.id}`, state: item })
+  }
+
+  return (
+    <Main>
+      <div className="title">推荐</div>
+      <Scroll
+        direction="vertical" // 垂直滚动
+        refresh={false} // 下拉更新为false
+        pullUp={handlePullUp}
+        pullDown={handlePullDown}
+        onScroll={
+          () => {
+            setTimeout(() => {
+              forceCheck()
+            }, 500)
+          }
+        }
+      >
+        <div className="container">
+          <div className="teslaFind">
+            <img src='https://china-community-app.tesla.cn/community-media/de6f422a-4c62-4a90-a5b0-c7460783bf4b.png' />
+          </div>
+          <div className="teslaNews">
+            {
+              newsList.map((item, index) => {
+                return (
+                  <div className="news" key={index} onClick={() => gotoNews(item)}>
+                    <div className="newsLeft">
+                      <h1>{item.title}</h1>
+                      <p>{item.date}</p>
+                    </div>
+                    <div className="newsRight">
+                      <Lazyload
+                        height={100}
+                        placeholder={
+                          <img width="100%" height="100%" src={loading} />
+                        }
+                      >
+                        <img src={item.picUrl} />
+                      </Lazyload>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </Scroll>
+      <div className={isLoading ? 'pullUpLoading' : ''}>
+        <img src={loading2} />
+      </div>
+      {/* 一定要开启子路由 */}
+      {renderRoutes(route.routes)}
+    </Main>
+  )
+}
+
+
+const mapStateToProps = (state) => {
+  return {
+    finddata: state.find.finddata
+  }
+}
+
+const mapStateToDispatch = (dispatch) => {
+  return {
+    getFindDataDispatch(page) {
+      dispatch(actionCreators.getFindData(page))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapStateToDispatch)(memo(Find))
